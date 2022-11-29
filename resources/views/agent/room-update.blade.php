0728fb656358207cc1123514c7200abad2f5db96
DESKTOP-126ANHP\CptFurball <elys.1993a@gmail.com>
2022-11-29 10:22:46 +0800

    polished room-update

--- resources/views/agent/room-update.blade.php
+++ resources/views/agent/room-update.blade.php
@@ -1,6 +1,33 @@
 <html>
     <h3>Room Update - Form</h3>
-    <form method="post" action="{{ route('agent.room-update' , $room->id) }}" enctype="multipart/form-data">
+
+</html>
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Reservations</title>
+    </head>
+
+    <body>
+        
+        @include('header')
+        
+        <div class="room" style="margin-top:150px">
+            <h1 style="font-size: 30px;color: black;font-weight: bold;text-align: center;">Rooms</h1>
+            <hr style="width:90%;border-top: 2px groove #8c8c8c;">
+        </div>
+
+        
+
+        <div class="container pb-4">
+        <form method="post" action="{{ route('agent.room-update' , $room->id) }}" enctype="multipart/form-data">
         {{ csrf_field() }}
 
         <table>
@@ -9,7 +36,7 @@
                     Room Title:
                 </td>
                 <td>
-                    <input name="room_title" type="text" value="{{ $room->room_title ?? old('room_title') }}">
+                    <input name="room_title" class="form-control" type="text" value="{{ $room->room_title ?? old('room_title') }}">
                     <p></p>
                 </td>
             </tr>
@@ -18,7 +45,7 @@
                     Room Type:
                 </td>
                 <td>
-                <select name="room_type" id="room_type" value="{{ $room->room_type }}">
+                <select name="room_type" id="room_type" value="{{ $room->room_type }}" class="form-select">
                     <option value="">Select Room Type</option>
                     <option value="big_room" {{ "big_room" == $room->room_type ? 'selected' : '' }} >Big Room</option>
                     <option value="medium_room" {{ "medium_room" == $room->room_type ? 'selected' : '' }}>Medium Room</option>
@@ -31,7 +58,7 @@
                     Room Description:
                 </td>
                 <td>
-                    <textarea name="room_desc" rows="4" cols="50" value="{{ $room->room_desc ?? old('room_desc') }}">{{ $room->room_desc ?? old('room_desc') }}</textarea>
+                    <textarea name="room_desc" rows="4" cols="50" value="{{ $room->room_desc ?? old('room_desc') }}" class="form-control">{{ $room->room_desc ?? old('room_desc') }}</textarea>
                 </td>
             </tr>
             <tr>
@@ -39,7 +66,7 @@
                     Monthly Rental:
                 </td>
                 <td>
-                    <input name="monthly_rental" type="text" value="{{ $room->monthly_rental ?? old('monthly_rental') }}">
+                    <input name="monthly_rental" type="text" value="{{ $room->monthly_rental ?? old('monthly_rental') }}" class="form-control">
                 </td>
             </tr>
             <tr>
@@ -47,7 +74,7 @@
                     Deposit:
                 </td>
                 <td>
-                <input name="deposit" type="text" value="{{ $room->deposit ?? old('deposit') }}">
+                <input name="deposit" type="text" value="{{ $room->deposit ?? old('deposit') }}" class="form-control">
                 </td>
             </tr>
             <tr>
@@ -55,8 +82,8 @@
                     Image:
                 </td>
                 <td>
-                    <img src='{{ $room->image }}' style="width:320px; height:240px;">
-                    <input name="image" type="file" value="{{ $room->image ?? old('image') }}">
+                    <img src='{{ $room->image }}' style="width:440px; height:240px;">
+                    <input name="image" type="file" value="{{ $room->image ?? old('image') }}" class="form-control">
                 </td>
             </tr>
             <tr>
@@ -64,10 +91,14 @@
                     Remark:
                 </td>
                 <td>
-                    <input name="remark" type="text" value="{{ $room->remark ?? old('remark') }}">
+                    <input name="remark" type="text" value="{{ $room->remark ?? old('remark') }}" class="form-control">
                 </td>
             </tr>
         </table>
-        <button type="submit">Update</button>
+        <button type="submit" class="btn btn-primary">Update</button>
     </form>
+        </div>
+    </body>
+    
+    @include('footer')
 </html>
